f12ee40df3082c84eea167ada9765090ab584b74
paploxd-svg <paploxd@gmail.com>
2026-02-10 19:10:35 -0800

    guardando mi sesion de python basico

---
+++ sesion_01.py
@@ -0,0 +1,14 @@
+# numericos y flotantes
+print(int(7))
+print(float(7.7))
+print(type(7))
+print(type(7.77))
+print(int(1+2))
+print(int(10*2))
+print("============operadores matematicos=========")
+#+
+#-
+#*
+#/
+# **
+# % modulo
