5a5263e0600fe9840ad18d376ea3ef30faab2e98
paploxd-svg <paploxd@gmail.com>
2026-02-12 18:35:44 -0800

    pide algo para ver si es par o impar

---
+++ sesion_02.py
@@ -0,0 +1,9 @@
+#tuplas 
+mi_tupla= (2,4)
+print ("mi tupla: ", mi_tupla)
+
+#lista 
+mi_lista = [1, 3.1416, "Pablo ", mi_tupla]
+print("El peimwe elemento de mi lista ", mi_lista[0])
+print("El cuarto elemento de mi lista ", mi_lista[3])
+print("El tercer elemento de mi lista ", mi_lista[2])
