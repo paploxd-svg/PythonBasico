70091640be9ac2100fabb4cdff32294b727da232
paploxd-svg <paploxd@gmail.com>
2026-02-13 15:27:50 -0800

    nose que poner

--- sesion_03.py
+++ sesion_03.py
@@ -6,13 +6,24 @@ for i in mi_li:
 
     resul = 0
     for i in mi_li:
-        resul += i
+         resul += i
 print(f"El resultado de la suma de mi lista es: {resul} ")
 
 for i in range(2,9):
-            print(i)
+                print(i)
 
 mi_lista_2 = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
 for i in mi_lista_2:
-       if i != "Lunes":
-              print(f"Feliz{i}!")
+        if i != "Lunes":
+                print(f"Feliz{i}!")
+
+#while loop
+i= 0
+
+while i< 5:
+    i += 1
+    if i== 3:
+        continue 
+    print(i)
+else:
+    print("i es ahora mayor o igual a 5")
