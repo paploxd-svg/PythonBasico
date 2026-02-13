b1c97644416c33bd6fb799a28845e989a1667bee
paploxd-svg <paploxd@gmail.com>
2026-02-12 19:24:28 -0800

    pide algo para ver si es par o impar

--- sesion_03.py
+++ sesion_03.py
@@ -11,3 +11,8 @@ print(f"El resultado de la suma de mi lista es: {resul} ")
 
 for i in range(2,9):
             print(i)
+
+mi_lista_2 = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
+for i in mi_lista_2:
+       if i != "Lunes":
+              print(f"Feliz{i}!")
