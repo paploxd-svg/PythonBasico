a90b1aa7a70549d8bed8922de59dc6b6449c4e97
paploxd-svg <paploxd@gmail.com>
2026-02-13 15:51:26 -0800

    nose que poner

--- sesion_03.py
+++ sesion_03.py
@@ -12,6 +12,8 @@ print(f"El resultado de la suma de mi lista es: {resul} ")
 for i in range(2,9):
                 print(i)
 
+
+
 mi_lista_2 = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
 for i in mi_lista_2:
         if i != "Lunes":
@@ -25,5 +27,35 @@ while i< 5:
     if i== 3:
         continue 
     print(i)
+    if i == 4:
+        break
 else:
     print("i es ahora mayor o igual a 5")
+
+    #practica 2
+    #dada la lista mi_lista_2 = ["lunes", "martes", "miercoles", "jueves", "viernes"]
+    #imprimir cada elemento de la lista 3 veces y cuando sea lunes no lo incluyas
+    # pista: usa los dos tipo loops while y for en el mismo programa para lograrlo
+    # resultado:
+    # martes
+    # miercoles
+    # jueves 
+    # viernes 
+    # martes
+    # miercoles
+    # jueves 
+    # viernes 
+    # martes
+    # miercoles
+    # jueves 
+    # viernes 
+
+i = 0
+mi_lista_2 = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
+while i< 3:
+    i += 1
+ 
+    for d in mi_lista_2:
+        if d != "Lunes":
+           print(d)
+              
