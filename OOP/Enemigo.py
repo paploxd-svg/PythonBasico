f41b04065e08a5ae96c0980b4485c6451b80b21e
paploxd-svg <paploxd@gmail.com>
2026-02-26 18:06:25 -0800

    f

---
+++ OOP/Enemigo.py
@@ -0,0 +1,23 @@
+class Enemigo:
+    tipo_enemigo: str
+    punto_energia: int = 10
+    ataque = 1
+
+    def __unit__(self, tipo_enemigo, punto_energia=10, ataque=1):
+        self.__tipo_enemigo = tipo_enemigo
+        self.punto_energia = punto_energia
+        self.ataque = ataque
+
+    def get_tipo_enemigo(self):
+        return self.__tipo_enemigo
+        
+    def habla(self):
+        print(f"yo son {self.__tipo_enemigo}. Preparado para pelear!!")
+
+    def camina(self):
+        print(f"{self.__tipo_enemigo} se mueve cerca de ti!!")
+
+    def ataca(self):
+        print(f"{self.__tipo_enemigo} ataca con un {self.ataque} de daño!!")
+
+              
